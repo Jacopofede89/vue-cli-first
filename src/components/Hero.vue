0c640d6e1c9299a068fc696ba5f86d615232504f
Jacopofede89 <jacopofede89@gmail.com>
2021-11-23 18:23:38 +0100

    ex finale

---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+    <img src="https://i1.wp.com/www.giacomocusano.com/wp-content/uploads/2016/07/coastal-wash-web.jpg?fit=1024%2C682&ssl=1" alt="">
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
